4bf9160ca607575061295409137b20033fecd3ee
Roland Clairquin <roland.clairquin@aeronomie.be>
2016-09-12 13:26:40 +0200

    Problem 0003

---
+++ clojure/problem_0003.clj
@@ -0,0 +1,39 @@
+;;(ns euler)
+
+;; The prime factors of 13195 are 5, 7, 13 and 29.
+;; What is the largest prime factor of the number 600851475143 ?
+
+(defn prime?
+  [n]
+  {:pre [(integer? n) (> n 0)]}
+  (or (== 2 n)
+      (let [mmax (+ (Math/sqrt n) 1)]
+        (not (some #(== 0 (rem n %)) (concat [2] (range 3 mmax 2))))
+        )))
+
+(defn lz-primes
+  []
+  (concat [2]
+          (filter prime? (iterate #(+ 2 %) 3))))
+
+
+(defn prime-factors
+  [n]
+  {:pre [(integer? n) (> n 0)]} 
+
+  (letfn
+   [(next-fact [n] (if (== n 2) 3 (+ n 2)))]
+
+   (loop [n n, m 2, factors []]
+     (let [mmax (+ (int (Math/sqrt n)) 1)]
+       (cond (> m mmax)       (conj factors n)
+             (== 0 (rem n m)) (recur (/ n m) m (conj factors m))
+             :else            (recur n (next-fact m) factors) )))))
+
+(last (prime-factors 600851475143))
+
+;;-> 6857
+
+
+
+
